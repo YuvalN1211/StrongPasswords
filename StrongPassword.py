f4467bc27081ce53f8d14249a346ee6f9708823a
YuvalN1211 <yuvaln2010@gmail.com>
2026-06-29 16:58:47 +0300

    General functions completed

--- StrongPassword.py
+++ StrongPassword.py
@@ -5,13 +5,45 @@ def InputPassword():
     else:
         return password
 
-def LengthCheck(password, current_points):
+def LengthCheck():
+    global password
     if len(password) < 8:
-        return current_points
+        return 0
     elif len(password) < 12:
-        return current_points + 1
+        return 1
     else:
-        return current_points + 2
+        return 2
 
-def ComplexityCheck(password, current_points)
-    pass
+def ComplexityCheck():
+    global password
+    #defining starting values for booleans
+    bool_dict = {"is_there_small_letters":False, "is_there_big_letters":False, "is_there_numbers":False, "is_there_special_charecters":False}
+    
+    for char in password:
+        ascii_value = ord(char)
+        if 97 <= ascii_value and ascii_value <= 122:
+            bool_dict["is_there_small_letters"] = True
+        elif 65 <= ascii_value and ascii_value <= 90:
+            bool_dict["is_there_big_letters"] = True
+        elif 48 <= ascii_value and ascii_value <= 57:
+            bool_dict["is_there_numbers"] = True
+        elif (33 <= ascii_value and ascii_value <= 47) or (58 <= ascii_value and ascii_value <= 64) or (91 <= ascii_value and ascii_value <= 96) or (123 <= ascii_value and ascii_value <= 126):
+            bool_dict["is_there_special_charecters"] = True
+    
+    points = 0
+    for value in bool_dict.values():
+        point += 1
+    return points
+
+def PopularWordsCheck():
+    global password
+    if "123" in password:
+        return -2
+    elif "admin" in password:
+        return -2
+    else:
+        return 0
+
+
+password = InputPassword()
+final_points = LengthCheck() + ComplexityCheck() + PopularWordsCheck()
